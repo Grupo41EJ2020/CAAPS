0f61d0b29b7e8f1a00df229b0e16581ea465d6d5
Aylin Zarate <aylinbieber8@hotmail.com>
2020-05-28 00:37:13 -0500

    Tema Terminado

--- Controllers/TemaController.cs
+++ Controllers/TemaController.cs
@@ -4,16 +4,73 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
+using MVCLaboratorio.Models;
+using System.Data;
+using System.Data.SqlClient;
+using MVCLaboratorio.Utilerias;
+
 namespace MVCLaboratorio.Controllers
 {
     public class TemaController : Controller
     {
-        //
+        RepositorioTema obtenerTemas = new RepositorioTema();
         // GET: /Tema/
+
+
+        //LISTA
 
         public ActionResult Index()
         {
+            return View(obtenerTemas.obtenerTemas());
+        }
+
+        //DETALLE
+
+        public ActionResult Details(int id)
+        {
+            return View(obtenerTemas.obtenerTema(id));
+        }
+
+        //DELETE
+
+        public ActionResult deleteTema(int id)
+        {
+            return View(obtenerTemas.obtenerTema(id));
+        }
+        [HttpPost]
+        public ActionResult deleteTema(int id, FormCollection datos)
+        {
+            obtenerTemas.eliminarTema(id);
+
+            return RedirectToAction("Borrado");
+        }
+        
+
+        //CREATE
+        public ActionResult createTema()
+        {
             return View();
+        }
+        [HttpPost]
+        public ActionResult createTema(Tema datosTemadatosTema)
+        {
+            obtenerTemas.insertarTema(datosTemadatosTema);
+            return RedirectToAction("Creado");
+        }
+
+
+        //UPDATE
+        public ActionResult updateTema(int id)
+        {
+            return View(obtenerTemas.obtenerTema(id));
+        }
+
+        [HttpPost]
+        public ActionResult updateTema(int id, Tema datosTema)
+        {
+            datosTema.IdTema = id;
+            obtenerTemas.actualizarTema(datosTema);
+            return RedirectToAction("Modificado");
         }
 
     }
---
+++ Views/Tema/Details.aspx
@@ -0,0 +1,27 @@
+﻿<%@ Page Language="C#" Inherits="System.Web.Mvc.ViewPage<MVCLaboratorio.Models.Tema>" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" >
+<head runat="server">
+    <title>Detalles</title>
+</head>
+<body>
+    <fieldset>
+        <legend>Información Tema</legend>
+        
+        <div class="display-label">IdTema</div>
+        <div class="display-field"><%: Model.IdTema %></div>
+        
+        <div class="display-label">Nombre</div>
+        <div class="display-field"><%: Model.Nombre %></div>
+        
+    </fieldset>
+    <p>
+        <%: Html.ActionLink("Modificar", "updateTema", new { id=Model.IdTema }) %> |
+        <%: Html.ActionLink("Volver a la lista", "Index") %>
+    </p>
+
+</body>
+</html>
+
